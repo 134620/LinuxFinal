1e0d1ccbc04b510aaf720640616c994cd07e3707
liuyating <1779849981@qq.com>
2021-11-13 16:24:51 +0800

    java

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student{
+   private String name;
+   private String number;
+   private int age;
+   private String sex;
+   Student(String name,String number,int age,String sex){
+      this.name = name;
+      this.number = number;
+      this.age = age;
+      this.sex = sex;
+   }
+}
